a05a1e772729ab79673acc27afd36c9be6d580e3
Matthew Savage <matt@amasses.net>
2014-08-25 22:18:19 +1000

    Version bump to 2.1.3

--- FatZebra/Properties/AssemblyInfo.cs
+++ FatZebra/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fat Zebra")]
 [assembly: AssemblyProduct("FatZebra .NET Wrapper")]
-[assembly: AssemblyCopyright("Copyright © Fat Zebra 2012")]
+[assembly: AssemblyCopyright("Copyright © Fat Zebra from 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1")]
-[assembly: AssemblyFileVersion("2.1.1")]
+[assembly: AssemblyVersion("2.1.3")]
+[assembly: AssemblyFileVersion("2.1.3")]
